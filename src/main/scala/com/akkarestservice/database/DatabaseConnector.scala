9f13436aa38dee8aec913d69644e8ce42b4f4d72
michael2109 <michael.jhaywoods@gmail.com>
2019-08-06 21:54:08 +0100

    Added h2 and slick.

--- src/main/scala/com/akkarestservice/database/DatabaseConnector.scala
+++ src/main/scala/com/akkarestservice/database/DatabaseConnector.scala
@@ -3,18 +3,14 @@ package com.akkarestservice.database
 // Use H2Profile to connect to an H2 database
 import slick.jdbc.H2Profile.api._
 
+import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
 
 
 class DatabaseConnector {
 
   val db = Database.forConfig("h2mem1")
-  try {
-    // val resultFuture: Future[_] = { ... }
-    //  Await.result(resultFuture, Duration.Inf)
-    //  lines.foreach(Predef.println _)
-  } finally db.close
-
 
   // Definition of the SUPPLIERS table
   class Suppliers(tag: Tag) extends Table[(Int, String, String, String, String, String)](tag, "SUPPLIERS") {
@@ -56,38 +52,32 @@ class DatabaseConnector {
   val coffees = TableQuery[Coffees]
 
   val setup = DBIO.seq(
-    // Create the tables, including primary and foreign keys
+
     (suppliers.schema ++ coffees.schema).create,
 
-    // Insert some suppliers
     suppliers += (101, "Acme, Inc.", "99 Market Street", "Groundsville", "CA", "95199"),
     suppliers += (49, "Superior Coffee", "1 Party Place", "Mendocino", "CA", "95460"),
     suppliers += (150, "The High Ground", "100 Coffee Lane", "Meadows", "CA", "93966"),
-    // Equivalent SQL code:
-    // insert into SUPPLIERS(SUP_ID, SUP_NAME, STREET, CITY, STATE, ZIP) values (?,?,?,?,?,?)
 
-    // Insert some coffees (using JDBC's batch insert feature, if supported by the DB)
-    coffees ++= Seq(
-      ("Colombian", 101, 7.99, 0, 0),
-      ("French_Roast", 49, 8.99, 0, 0),
-      ("Espresso", 150, 9.99, 0, 0),
-      ("Colombian_Decaf", 101, 8.99, 0, 0),
-      ("French_Roast_Decaf", 49, 9.99, 0, 0)
-    )
-    // Equivalent SQL code:
-    // insert into COFFEES(COF_NAME, SUP_ID, PRICE, SALES, TOTAL) values (?,?,?,?,?)
+    coffees += ("Colombian", 101, 7.99, 0, 0),
+    coffees += ("French_Roast", 49, 8.99, 0, 0),
+    coffees += ("Espresso", 150, 9.99, 0, 0),
+    coffees += ("Colombian_Decaf", 101, 8.99, 0, 0),
+    coffees += ("French_Roast_Decaf", 49, 9.99, 0, 0)
   )
 
-  val setupFuture = db.run(setup)
+  try {
+    //  val setupFuture = db.run(setup)
+    Await.result(db.run(setup), Duration.Inf)
 
-  // Read all coffees and print them to the console
-  println("Coffees:")
-  db.run(coffees.result).map(_.foreach {
-    case (name, supID, price, sales, total) =>
-      println("  " + name + "\t" + supID + "\t" + price + "\t" + sales + "\t" + total)
-    case other => println(other)
-  })
-  println("Coffees printed")
-  // Equivalent SQL code:
-  // select COF_NAME, SUP_ID, PRICE, SALES, TOTAL from COFFEES
+    // Read all coffees and print them to the console
+    println("Coffees:")
+
+    db.run(coffees.result).map(_.foreach {
+      case (name, supID, price, sales, total) =>
+        println("  " + name + "\t" + supID + "\t" + price + "\t" + sales + "\t" + total)
+      case other => println(other)
+    })
+  } finally db.close
+
 }
